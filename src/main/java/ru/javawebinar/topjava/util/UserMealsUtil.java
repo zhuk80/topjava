eb926a356188aa787dcd8df840eb088916805dac
Evgeniy <zhuk_8080@mail.ru>
2017-03-19 11:05:38 +0800

    За указанный временной промежуток выводятся записи за все дни, в случае превышения каллорий - флаг true, иначе - false.

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -28,7 +28,7 @@ public class UserMealsUtil {
         );
 
         for (UserMealWithExceed userMealWithExceed : getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000)) {
-            System.out.println(userMealWithExceed.getDateTime() + " " + userMealWithExceed.getDescription() + " " + userMealWithExceed.getCalories());
+            System.out.println(userMealWithExceed.getDateTime() + " " + userMealWithExceed.getDescription() + " " + userMealWithExceed.getCalories() + " " + userMealWithExceed.isExceed());
         }
 
     }
@@ -40,34 +40,35 @@ public class UserMealsUtil {
                         .collect(Collectors.groupingBy(meal -> meal.getDateTime().toLocalDate(), Collectors.summingInt(UserMeal::getCalories)));
 
         return mealList.stream()
-                .filter(meal -> allCaloriesByDay.get(meal.getDateTime().toLocalDate()) > caloriesPerDay && TimeUtil.isBetween(meal.getDateTime().toLocalTime(), startTime, endTime))
-                .map(meal -> new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(), true))
+                .filter(meal -> TimeUtil.isBetween(meal.getDateTime().toLocalTime(), startTime, endTime))
+                .map(meal -> allCaloriesByDay.get(meal.getDateTime().toLocalDate()) > caloriesPerDay ?
+                        new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(), true) :
+                        new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(), false))
                 .collect(Collectors.toList());
 
 
-        /*
-        То же, только на циклах:
-
-         Map<LocalDate, Integer> allCaloriesByDay = new HashMap<>();
-
+        /* Решение на циклах
+        Map<LocalDate, Integer> allCaloriesByDay = new HashMap<>();
 
         for (UserMeal userMeal : mealList) {
             LocalDate currentDate = userMeal.getDateTime().toLocalDate();
-            allCaloriesByDay.merge(currentDate, userMeal.getCalories(), (a, b) -> a + b);
+            allCaloriesByDay.merge(currentDate, userMeal.getCalories(), Integer::sum);
         }
 
         List<UserMealWithExceed> result = new ArrayList<UserMealWithExceed>();
 
         for (UserMeal userMeal : mealList) {
             LocalTime mealListTime = userMeal.getDateTime().toLocalTime();
-            if (TimeUtil.isBetween(mealListTime, startTime, endTime) && allCaloriesByDay1.get(userMeal.getDateTime().toLocalDate()) > caloriesPerDay) {
-                result.add(new UserMealWithExceed(userMeal.getDateTime(), userMeal.getDescription(), userMeal.getCalories(), true));
+            if (TimeUtil.isBetween(mealListTime, startTime, endTime))
+            {
+                result.add(allCaloriesByDay.get(userMeal.getDateTime().toLocalDate()) > caloriesPerDay ?
+                        new UserMealWithExceed(userMeal.getDateTime(), userMeal.getDescription(), userMeal.getCalories(), true) :
+                        new UserMealWithExceed(userMeal.getDateTime(), userMeal.getDescription(), userMeal.getCalories(), false));
             }
-        }
 
-        return result;
-}
-         */
+
+        }
+        return result;*/
 
 
     }
