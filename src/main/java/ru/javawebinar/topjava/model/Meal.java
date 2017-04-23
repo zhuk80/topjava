b49d2f0beb3ff83335c7e533e1f63faaa4d1db76
Evgeniy <zhuk_8080@mail.ru>
2017-04-23 21:44:29 +0800

    2_fix_hibernate_issue

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -104,7 +104,7 @@ public class Meal extends BaseEntity {
     @Override
     public String toString() {
         return "Meal{" +
-                "id=" + id +
+                "id=" + getId() +
                 ", dateTime=" + dateTime +
                 ", description='" + description + '\'' +
                 ", calories=" + calories +
