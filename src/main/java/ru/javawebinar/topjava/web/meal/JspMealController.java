01800b8e0f92e1f8cc6e66366db2820bc63458e6
Evgeniy <zhuk_8080@mail.ru>
2017-05-14 13:15:10 +0800

    03-HW6-fix-relative-url-utf8

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -23,7 +23,7 @@ public class JspMealController extends AbstractMealController {
     @RequestMapping(value = "/delete", method = RequestMethod.GET)
     public String delete(HttpServletRequest request) {
         super.delete(getId(request));
-        return "redirect:meals";
+        return "redirect:/meals";
     }
 
     @RequestMapping(value = "/update", method = RequestMethod.GET)
@@ -49,7 +49,7 @@ public class JspMealController extends AbstractMealController {
         } else {
             super.update(meal, getId(request));
         }
-        return "redirect:meals";
+        return "redirect:/meals";
     }
 
     @RequestMapping(value = "/filter", method = RequestMethod.POST)
