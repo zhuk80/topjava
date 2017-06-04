c713dd86683eddd2da37dc1d8e755a8f6b6b2160
Evgeniy <zhuk_8080@mail.ru>
2017-06-04 16:12:18 +0800

    11_global_exception

---
+++ src/main/java/ru/javawebinar/topjava/web/GlobalControllerExceptionHandler.java
@@ -0,0 +1,29 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import ru.javawebinar.topjava.AuthorizedUser;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalControllerExceptionHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+        LOG.error("Exception at request " + req.getRequestURL(), e);
+        ModelAndView mav = new ModelAndView("exception/exception");
+        mav.addObject("exception", e);
+
+        // Interceptor is not invoked, put userTo
+        AuthorizedUser authorizedUser = AuthorizedUser.safeGet();
+        if (authorizedUser != null) {
+            mav.addObject("userTo", authorizedUser.getUserTo());
+        }
+        return mav;
+    }
+}
